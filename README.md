bdac6f5c8b8e7f66e9634bcd371826cc670c6d85
lukefowles <luke.fowles15@gmail.com>
2021-10-13 10:32:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-cvhyom
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-cvhyom)
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'tachyons';
+import Card from './components/Card';
+import './index.css';
+
+
+ReactDOM.render(
+  <Card name="Luke" email="luke@fowlesmail.com"/>, 
+  document.getElementById('root'));
