f09005cc9447bb4d910e17ee792a844efb8e90f6
lukefowles <luke.fowles15@gmail.com>
2021-10-13 14:59:20 +0100

    1 file modified

--- src/users.js
+++ src/users.js
@@ -1,15 +1,1 @@
-import { useState, useEffect } from 'react';
 
-const [users, setUsers] = useState([]);
-
-useEffect(() => {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      setUsers(data);
-    });
-}, []);
-
-export default users;
